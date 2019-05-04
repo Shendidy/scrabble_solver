92ce3e48737ccb0cba0c22e202e8224e42783434
Shendidy <sherif@shendidy.com>
2019-05-04 13:20:04 +0100

    calculates score for any single character word

--- lib/scrable.rb
+++ lib/scrable.rb
@@ -1,8 +1,21 @@
 class Scrable
   def initialize(word = '')
     @word = word
+    @score_board = {
+      0 => ["", " ", " \t\n", nil],
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 => ["B", "C", "M", "P"],
+      4 => ["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]
+    }
   end
   def score
-    @word == 'a' ? 1 : 0
+    return 0 if ["", " ", " \t\n", nil].include? @word
+    @score_board.find{
+                       |k,v| return k if v.include? @word.upcase
+                     }.first
   end
 end
--- spec/scrable_spec.rb
+++ spec/scrable_spec.rb
@@ -1,27 +1,27 @@
 require 'scrable'
 
 describe Scrable do
-  it 'should return 0' do
+  it '1- should return 0' do
     game = Scrable.new("")
     expect(game.score).to eq(0)
   end
 
-  it 'should return 0' do
+  it '2- should return 0' do
     game = Scrable.new(" \t\n")
     expect(game.score).to eq(0)
   end
 
-  it 'should return 0' do
+  it '3- should return 0' do
     game = Scrable.new(nil)
     expect(game.score).to eq(0)
   end
 
-  it 'should return 1' do
+  it '4- should return 1' do
     game = Scrable.new("a")
     expect(game.score).to eq(1)
   end
 
-  it 'should return 4' do
+  it '5- should return 4' do
     game = Scrable.new("f")
     expect(game.score).to eq(4)
   end
