49bc0bbf81048d0c3c8f2cbbaf3af8124174a64b
Shendidy <sherif@shendidy.com>
2019-05-04 12:47:19 +0100

    testing different single letters

--- spec/scrable_spec.rb
+++ spec/scrable_spec.rb
@@ -16,8 +16,13 @@ describe Scrable do
     expect(game.score).to eq(0)
   end
 
-  it 'should return 0' do
+  it 'should return 1' do
     game = Scrable.new("a")
     expect(game.score).to eq(1)
+  end
+
+  it 'should return 4' do
+    game = Scrable.new("f")
+    expect(game.score).to eq(4)
   end
 end
