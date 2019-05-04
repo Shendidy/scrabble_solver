dec658f244c254444510f6ee131176feffad4060
Shendidy <sherif@shendidy.com>
2019-05-04 12:44:23 +0100

    first test with actual letter

--- spec/scrable_spec.rb
+++ spec/scrable_spec.rb
@@ -10,4 +10,14 @@ describe Scrable do
     game = Scrable.new(" \t\n")
     expect(game.score).to eq(0)
   end
+
+  it 'should return 0' do
+    game = Scrable.new(nil)
+    expect(game.score).to eq(0)
+  end
+
+  it 'should return 0' do
+    game = Scrable.new("a")
+    expect(game.score).to eq(1)
+  end
 end
